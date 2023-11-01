c1e03f0eb2021aac848a9a433e02fccd40b51181
dishap001 <pdisha@trellissoft.ai>
2023-11-01 12:48:23 +0530

    Changing the Player Name

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import React from "react"
+import Player from "./components/Player.jsx"
+
 
 function App() {
   
 
   return (
-    <div>
-      React Tic-Tac-Toe
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+        <Player initialName="Player1" symbol="X"/>
+        <Player initialName="Player2" symbol="O"/>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
     
-  )
+  );
 }
 
 export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
+function Player({initialName ,symbol}){
+
+    const [isEditing ,setIsEditing] = useState(false);
+    const [playerName,setPlayerName] = useState(initialName);
+
+    function handleEditClick (){
+        setIsEditing((editing) => !editing);
+
+    }
+    function handleChange(event){
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = <span className="player-name"  > {playerName}</span>;
+
+    
+    if(isEditing){
+        editablePlayerName = (<input type="text" required value={playerName} onChange={handleChange}/>);
+       // btnCaption="Save";
+    }
+    
+    return(
+
+        <li>
+        <span className="player">
+       {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing?'Save':'Edit'}</button>
+        </li>
+    );
+}
+export default Player;
